79c016210fe009e56c09d1af9f6163ded0005625
Steven <senorvalenz@gmail.com>
2025-06-19 16:25:27 -0400

    Working - one flower at a time

--- a_another-test/sketch.js
+++ a_another-test/sketch.js
@@ -1,43 +1,25 @@
 function setup() {
   createCanvas(400, 400);
+  frameRate(1);
 }
-
-let flower = {
-  x: 200,
-  y: 100,
-  emoji: "🌸",
-  fname: 'hiiiii'
-}
-
-class Flower {
-  constructor() {
-    this.x = random(0, width);
-    this.y = random(0, height);
-    this.emoji = "🌸";
-  }
-}
-
-class Garden {
-  constructor(num) {
-    this.plants = [];
-    for (let i = 0; i < num; i++) {
-      this.plants.push(new Flower())
-    }
-  };
-  
-  draw() {
-    this.plants.forEach(() => {
-      text(this.emoji, this.x, this.y);
-    })
-  }
-}
-
-let garden = new Garden(20);
-
-
 
 function draw() {
-  background("lightblue")
-  textSize(100);
-  garden.draw();
+  background("lightblue");
+
+  let myFlower = createFlower();
+  
+  fill(myFlower.color);
+  ellipse(myFlower.x, myFlower.y, myFlower.size);
+}
+
+
+function createFlower() {
+  let flower = {
+    x: random(20, 380),
+    y: random(20, 380),
+    size: random(20, 75),
+    lifespan: random(255, 300),
+    color: color(random(255), random(255), random(255)),
+  }
+  return flower;
 }
