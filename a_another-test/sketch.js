8a7f9bbed0bbec2b476a338de60b368c071ca78e
Steven <senorvalenz@gmail.com>
2025-06-19 16:15:23 -0400

    Attempt at making classes, not working

--- a_another-test/sketch.js
+++ a_another-test/sketch.js
@@ -1,3 +1,7 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
 let flower = {
   x: 200,
   y: 100,
@@ -5,13 +9,35 @@ let flower = {
   fname: 'hiiiii'
 }
 
-function setup() {
-  createCanvas(400, 400);
-  console.log(flower);
+class Flower {
+  constructor() {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.emoji = "🌸";
+  }
 }
+
+class Garden {
+  constructor(num) {
+    this.plants = [];
+    for (let i = 0; i < num; i++) {
+      this.plants.push(new Flower())
+    }
+  };
+  
+  draw() {
+    this.plants.forEach(() => {
+      text(this.emoji, this.x, this.y);
+    })
+  }
+}
+
+let garden = new Garden(20);
+
+
 
 function draw() {
   background("lightblue")
   textSize(100);
-  text(flower.emoji, flower.x, flower.y);
+  garden.draw();
 }
