aa9fa1876408b9490bbfff14fc3d7b493b744d13
Steven <senorvalenz@gmail.com>
2025-06-20 09:41:34 -0400

    OOP working! Incl. some appearance tweaks

--- a_another-test/sketch.js
+++ a_another-test/sketch.js
@@ -2,13 +2,14 @@
 
 function setup() {
   createCanvas(400, 400);
-  frameRate(1);
+  Flower.populate();
+  frameRate(20);
 }
 
 function draw() {
-  background("lightblue");
-  Flower.clear();  // Remove old flowers so they can be GC'ed
-  Flower.populate(100);
+  background("black");
+  // Flower.clear();  // Remove old flowers so they can be GC'ed
+  Flower.refresh(1);
   Flower.drawAll();
   
 }
@@ -17,13 +18,15 @@ class Flower {
   static instances = [];
   static MIN_SIZE = 20;
   static DISAPPEAR_SIZE = 5;
+  static MAX_INSTANCES = 100;
 
   constructor() {
     this.x = random(20, 380);
     this.y = random(20, 380);
     this.size = random(Flower.MIN_SIZE, 75);
+    this.shrink = random(85, 99)
     this.lifespan = random(255, 300);
-    this.color = color(random(255), random(255), random(255));
+    this.color = color(random(100,255), random(10), random(100, 255));
     
     Flower.instances.push(this);
   }
@@ -32,7 +35,7 @@ class Flower {
     this.instances = [];
   }
 
-  static populate(num) {
+  static populate(num = this.MAX_INSTANCES) {
     for (let i = 0; i < num; i++) {
       this.instances.push(new Flower());
     }
@@ -41,6 +44,12 @@ class Flower {
   static drawAll() {
     for (let f of this.instances) {
       f.draw();
+    }
+  }
+
+  static refresh(minimum) {
+    if (this.instances.length < minimum) {
+      this.populate(this.MAX_INSTANCES - minimum);
     }
   }
   
@@ -57,8 +66,12 @@ class Flower {
     ellipse(f.x, f.y, f.size / 2);
 
     // Shrink for next go round
-    
-    // Prune em
+    f.size = f.size - (f.shrink / 100);
 
+    // If too small, discard flower
+    if (f.size < Flower.DISAPPEAR_SIZE) {
+      let idx = Flower.instances.indexOf(f);
+      Flower.instances.splice(idx, 1);
+    }
   }
 }
