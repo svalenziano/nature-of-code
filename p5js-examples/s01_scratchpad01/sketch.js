39cea103b9c87b1f086b8ab3ca92de549de94e56
Steven <senorvalenz@gmail.com>
2025-06-24 15:11:53 -0400

    Success: Button.drawAll() and Button.handleAllClicks()

--- p5js-examples/s01_scratchpad01/sketch.js
+++ p5js-examples/s01_scratchpad01/sketch.js
@@ -1,6 +1,8 @@
 class Button {
   #x; #y; #w; #h; #onClick; #hover;
 
+  static instances = [];
+
   constructor(x, y, w, h, label, onClick) {
     this.#x = x;
     this.#y = y;
@@ -9,34 +11,51 @@ class Button {
     this.label = label;
     this.#onClick = onClick;  // callback func
     this.#hover = false;
+    Button.instances.push(this);
   }
 
   // call in your draw loop
-  draw() {
-    // check hover state
-    this.#hover = (
-      mouseX >= this.#x && mouseX <= this.#x + this.#w &&
-      mouseY >= this.#y && mouseY <= this.#y + this.#h
-    );
+  static drawAll() {
+    for (let i of Button.instances) {
+      // check hover state
+      i.#hover = (
+        mouseX >= i.#x && mouseX <= i.#x + i.#w &&
+        mouseY >= i.#y && mouseY <= i.#y + i.#h
+      );
 
-    // button background
-    noStroke();
-    fill(this.#hover ? 200 : 170);
-    rect(this.#x, this.#y, this.#w, this.#h, 5);
+      // button background
+      noStroke();
+      fill(i.#hover ? 200 : 170);
+      rect(i.#x, i.#y, i.#w, i.#h, 5);
 
-    // label
-    fill(50);
-    textAlign(CENTER, CENTER);
-    textSize(14);
-    text(this.label, this.#x + this.#w/2, this.#y + this.#h/2);
+      // label
+      fill(50);
+      textAlign(CENTER, CENTER);
+      textSize(14);
+      text(i.label, i.#x + i.#w/2, i.#y + i.#h/2);
+    }
   }
 
   // call in mousePressed()
-  handleClick() {
-    if (this.#hover && this.#onClick) {
-      this.#onClick();
+  static handleAllClicks() {
+    for (let i of Button.instances) {
+      if (i.#hover && i.#onClick) {
+        i.#onClick();
+      }
     }
   }
+}
+
+class ButtonArray {
+  /* 
+  STATIC FIELDS
+    - 
+  PROPS
+  - 
+  METHODS
+  - constructor:
+    - creates array of 
+  */
 }
 
 // ---------- sketch code ----------
@@ -45,17 +64,23 @@ let btn;
 
 function setup() {
   createCanvas(400, 300);
-  // instantiate a button at (150,50), size 100×40
-  btn = new Button(150, 50, 100, 40, 'PRESS', () => {
-    console.log('Button clicked!');
-  });
+
+  // Todo: encapsulate in a method that creates an array of buttons
+  // create buttons
+  let yOffset = 10;
+  let xOffset = 10;
+  let height = 25;
+  for (let i of ['Hello', 'World']) {
+    new Button(xOffset, yOffset, 150, height, i, () => console.log(i + ' clicked!'));
+    yOffset += yOffset + height;
+  }
 }
 
 function draw() {
   background(240);
-  btn.draw();
+  Button.drawAll();
 }
 
 function mousePressed() {
-  btn.handleClick();
+  Button.handleAllClicks();
 }
