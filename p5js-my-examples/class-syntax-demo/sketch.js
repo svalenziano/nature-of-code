c987ab77e4a72050fe06c64b955c7b866211eb1a
Steven <senorvalenz@gmail.com>
2025-07-11 15:05:04 -0400

    `Square` implementation WIP

--- p5js-my-examples/class-syntax-demo/sketch.js
+++ p5js-my-examples/class-syntax-demo/sketch.js
@@ -1,20 +1,43 @@
 "use strict";
 
+class Orchestrator {
+  constructor() {
+    this.instances = [];
+  }
 
+  createShapes(num=100) {
+    for (let i = 0; i < num; i++) {
+      this.instances.push(new Circle());
+    }
+  }
+
+  drawInstances() {
+    for (let i of this.instances) {
+      i.drawWrapper();
+    }
+  }
+}
 
 class Shape {
+  
+  static MIN_SIZE = 10;
+  static MAX_SIZE = 50;
+
+  // "public class field" (these become instance properties)
+  strokeWeight = 0.5;
+  strokeColor = color(10);
+  pos = createVector(CENTER.x, CENTER.y);
+  fill = color(255, 255, 255, 150);
+
+  static randomSize() {
+    return random(Shape.MIN_SIZE, Shape.MAX_SIZE);
+  }
+
   constructor() {
-    // position
-    this.pos = createVector(CENTER.x, CENTER.y);
-    // movement
-    this.speed = random(0.5, 1);
-    this.azimuth = createVector(1,1);
+    this.resetPosition();
+    this.randomizeSpeed()
+    this.azimuth = createVector(1, 1).rotate(random(0, 360));
     this.rotation = Math.floor(random(0, 360));
-    // appearance
-    this.fill = color(255, 255, 255);
-    this.alpha = 1;
-    this.strokeWeight = 0.5;
-    this.strokeColor = color(10);
   }
   
   // these are actions that should happen with every call to `draw`
@@ -25,13 +48,19 @@ class Shape {
     strokeWeight(this.strokeWeight);
     stroke(this.strokeColor);
     translate(this.pos.x, this.pos.y);
+    let scaleFactor = this.getScaleFactorByDist(0.1, 1, 0, width/2, CENTER);
+    scale(scaleFactor)
     this.draw();
     pop();
     this.move();
   }
 
+  randomizeSpeed() {
+    this.speed = random(0.1,0.5);
+  }
+
   draw() {
-    circle(0, 0, 10);
+    circle(0, 0, 50);
   }
 
   move() {
@@ -42,28 +71,92 @@ class Shape {
   }
 
   isOffScreen() {
-    return (this.pos.x < 0 ||
-            this.pos.x > width ||
-            this.pos.y < 0 ||
-            this.pos.y > height);
+    /* 
+    What is the role of `azimuth`?
+      If shape starts off screen, you should not reset it immediately.
+      CHOSEN METHOD: You must be able to tell if the shape is moving towards or away from the screen
+      ALTERNATIVE: (Not used) You must track whether or not a shape has been on screen
+    */
+    return ((this.pos.x < 0      - this.getSize() && this.azimuth.x < 0)||
+            (this.pos.x > width  + this.getSize() && this.azimuth.x > 0)||
+            (this.pos.y < 0      - this.getSize() && this.azimuth.y < 0)||
+            (this.pos.y > height + this.getSize() && this.azimuth.y > 0));
   }
 
   resetPosition() {
     this.pos.x = CENTER.x;
     this.pos.y = CENTER.y;
   }
+
+  getSize() {
+    // Different `Shape` subclasses may have different `size` properties:
+    return this.size ?? this.diameter ?? this.width ?? this.height ?? 75;
+  }
+
+  getScaleFactorByDist(
+    minScale=0.1, 
+    maxScale=1, 
+    minDist, 
+    maxDist, 
+    from=CENTER) {
+    /* 
+    minScale = minimum scale factor.  eg: 0.1 = 10% scale
+    maxScale = maximum scale factor.  eg: 1 = full scale
+    minDist = distance at which min scale factor should be applied
+    maxDist = ditto, except max scale factor
+    from = location (p5 vector) to measure from
+
+    RETURN = Scale factor, mapped to minScale <-> maxScale
+    */
+    let distance = from.dist(this.pos);
+    let scale = map(distance, minDist, maxDist, minScale, maxScale, false);
+    return scale;
+  }
+}
+
+class Circle extends Shape {
+  constructor(diameter = -1) {
+    super();
+    this.initializeDiameter(diameter);
+    
+  }
+
+  draw() {
+    circle(0, 0, this.diameter);
+  }
+
+  initializeDiameter(diameter) {
+    if (diameter <= 0) {
+      this.diameter = Shape.randomSize();
+    } else {
+      this.diameter = diameter;
+    }
+  }
+}
+
+class Square extends Shape {
+  
+  constructor(size) {
+    super();
+    this.size = size;
+  }
+
+  draw() {
+    rect(0, 0, this.size)
+  }
 }
 
 let CENTER;
-let s;
+let o;
 
 function setup() {
   createCanvas(400, 400);
   CENTER = createVector(width / 2, height / 2);
-  s = new Shape();
+  o = new Orchestrator();
+  o.createShapes(100);
 }
 
 function draw() {
   background(220);
-  s.drawWrapper();
+  o.drawInstances();
 }
