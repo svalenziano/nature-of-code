b71922d9a7926b124c2010942758ff575db54d35
Steven <senorvalenz@gmail.com>
2025-09-12 17:06:17 -0400

    extract coords into class

--- p5js-my-examples/my-open-street-map/sketch.js
+++ p5js-my-examples/my-open-street-map/sketch.js
@@ -1,7 +1,12 @@
+class TestCoordinates {
+  static coords = {
+    Taipei: [25.029928, 121.470337, 25.054501, 121.499004]
+  }
+}
+
 let data = null;
-const coords = [25.029928, 121.470337, 25.054501, 121.499004];
+const coords = TestCoordinates.coords.Taipei;
 const [latMin, longMin, latMax, longMax] = coords;
-let shapesDrawn = 0;
 
 function setup() {
     createCanvas(800, 800);
@@ -23,13 +28,10 @@ function draw() {
                 let y = map(p.lat, latMin, latMax, height, 0);
                 let x = map(p.lon, longMin, longMax, 0, width);
                 vertex(x, y);
-                console.log(x, y)
             })
             endShape(CLOSE);
         })
         noLoop();
-    } else {
-      console.error(`"ERROR: elements" not found in OSM data`)
     }
 }
 
