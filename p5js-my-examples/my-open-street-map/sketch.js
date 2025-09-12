c48721e19563707319a28658b10fee2101575083
Steven <senorvalenz@gmail.com>
2025-09-12 16:18:00 -0400

    Success - minimal draw of Taipei

--- p5js-my-examples/my-open-street-map/sketch.js
+++ p5js-my-examples/my-open-street-map/sketch.js
@@ -1,9 +1,13 @@
 let data = null;
+const coords = [25.029928, 121.470337, 25.054501, 121.499004];
+const [latMin, longMin, latMax, longMax] = coords;
+let shapesDrawn = 0;
 
 function setup() {
     createCanvas(800, 800);
     background(245);
-    let overpassUrl = 'https://overpass-api.de/api/interpreter?data=[out:json];(way["building"](35.64,139.7,35.73,139.8););out%20geom;';
+    let overpassUrl = `https://overpass-api.de/api/interpreter?data=[out:json];(way["building"](${coords.join(",")}););out%20geom;`;
+    console.log(overpassUrl);
     data = loadJSON(overpassUrl, gotData);
     noFill();
     strokeWeight(0.5);
@@ -16,13 +20,16 @@ function draw() {
         data["elements"].forEach(building => {
             beginShape();
             building["geometry"].forEach(p => {
-                let y = map(p.lat, 35.64, 35.73, height, 0);
-                let x = map(p.lon, 139.7, 139.8, 0, width);
+                let y = map(p.lat, latMin, latMax, height, 0);
+                let x = map(p.lon, longMin, longMax, 0, width);
                 vertex(x, y);
+                console.log(x, y)
             })
             endShape(CLOSE);
         })
         noLoop();
+    } else {
+      console.error(`"ERROR: elements" not found in OSM data`)
     }
 }
 
