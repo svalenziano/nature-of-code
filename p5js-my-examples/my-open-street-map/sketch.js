bdfb754106066309aaea6611f894b75b7f076375
Steven <senorvalenz@gmail.com>
2025-09-17 09:57:46 -0400

    Offline placeholder data - working

--- p5js-my-examples/my-open-street-map/sketch.js
+++ p5js-my-examples/my-open-street-map/sketch.js
@@ -8,59 +8,13 @@ HEAVILY INSPIRED BY: Prettymaps, by Marcelo de Oliveira Rosa Prates (https://git
 Dependencies: p5js library
 */
 
-/////////////////////////////////////////////////////////////////////////////
-// Slow Fetcher
-// Written in pre-ES6 syntax
-// There are more robust methods of throttling, but this does the trick for now
 
-/*
-  Since fetch is async and request may be resolved or rejected IN ANY ORDER (not the order that they were made), you must use a 'token-based' system rather than a linear data structure such as a queue.
-  
-  V1
-    IDEA: use Promise.withResolvers() to create a remote-controlled Promise.  Fetching is handled on a timer, and results / errors are resolved to the original promise.  
-    
-    STATES:
-      - Timer exists AND fetches in queue:
-        - push AND RETURN a future fetch to the queue!
-      - Else (Timer doesn't exist or queue empty):
-        - create timer
-        - fetch now AND RETURN
-      - Timer callback:
-        - if queue exists:
-          - dequeue and process one of the objects with a native `fetch`
-          - resolve /reject the associated `futureFetch` 
-        - else:
-          - clear the timer (access via this.timer)
-    ALGO
-      FETCH
-        - input = standard fetch args
-        - return = promise that resolves with a Response object (same as fetch)
-        REGARDLESS:
-          - make a promise (to make a request in the future) using `Promise.withResolvers`
-        if timer exists
-          - enqueue object: {resolver, rejector, url, options} => this object enables the promise to be resolved remotely, with the results of a "future fetch"
-          - return the future fetch promise (so that this function can be used identically to built-in `fetch`)
-        Else (timer is null), the request can be made immediately. 
-          - return the promise
-          - create the timer
-            - schedule the "future fetch" => create `processQueue` (helper) timer using setInterval
-      
-      processQueue (HELPER callback for setInterval)
-        - on timeout (execute every X milliseconds):
-          - if queue has elements:
-            - await: fetch and get json from response
-            - if response, resolve the original promise
-            - if error, reject
-          - else:
-            - clearInterval
 
-  V2
-    IDEA: use all.Settled to create a remote-controlled promise? I don't think this is possible.
-*/
 
 ///////////////////////////////////////////////////////////
 // CLASSES AND HELPER FUNCTIONS
 
+// There are more robust methods of throttling, but this does the trick for now
 class SlowFetcher {
   constructor(milliseconds) {
     this.queue = [];
@@ -78,14 +32,16 @@ class SlowFetcher {
     });
     
     // IF TIMER EXISTS, PUSH ARGUMENTS TO QUEUE FOR FUTURE FETCHING
-    if (this.timer) {
-
+    if (this.timer !== null) {
       this.queue.push({url, options, resolve, reject});
+      console.log("SlowFetcher: Pushing new fetch to queue")
+      console.log(this.queue)
       return futureFetch;
 
     // IF NO TIMER EXISTS, CREATE THE TIMER AND PROCESS THIS FETCH IMMEDIATELY
     } else {
       // CREATE TIMER that resolves the `futureFetch`
+      console.log("SlowFetcher: Creating timer")
       this.timer = setInterval(async () => {
         if (this.queue.length > 0) {
           const {url, options, resolve, reject} = this.queue.shift();
@@ -96,6 +52,7 @@ class SlowFetcher {
             reject(response);
           }
         } else {
+          console.log("SlowFetcher: Destroying timer")
           clearInterval(this.timer);
           this.timer = null;
         }
@@ -105,65 +62,28 @@ class SlowFetcher {
       // no `await`, since we want this method to act exactly like the native `fetch`
       return fetch(url, options);  
     }
-    
   }
 }
-
-
-class TestCoordinates {
-  static coords = {
-    Taipei: [25.029928, 121.470337, 25.054501, 121.499004],
-    Durham: [35.985577, -78.913336, 36.004673, -78.888788],
-  }
-}
-
 
 
 function setupListeners() {
   document.body.addEventListener("click", async (ev) => {
     console.log("Fetching data...")
-    renderTile(coords, await fetchLayer(coords, [myQueries.building]));
+    renderTile(coords, await fetchLayer(coords, [myQueries.building]), null);
   })
   console.log("Click to load: setup complete.");
 }
 
-async function fetchLayer(coords, queries) {
-  /*
-  coords: OSM-formatted array of coords, eg [25.029928, 121.470337, 25.054501, 121.499004]
-  queries: array of one or more Overpass API queries eg [`wr["building"];`, `wr["landuse"];`]
-  Return: json API response
-  */
-  try {
-    const coordString = coords.join(",")
-    const osmQuery = "data=" + encodeURIComponent(`
-        [bbox:${coordString}][out:json][timeout:${TIMEOUT}];
-        (
-          ${queries.join('')}
-        );
-        out geom;`);
 
-    const response = await fetch("https://overpass-api.de/api/interpreter", {
-      method: "POST",
-      body: osmQuery,
-    });
 
-    const json = await response.json();
-    return json;
-
-  } catch (error) {
-    console.log("Fetch from OSM failed");
-    throw error;
-  }
-}
-
-function renderTile([latMin, longMin, latMax, longMax], json) {
+function renderTile([latMin, longMin, latMax, longMax], json, color) {
     json.elements.forEach((object) => {
       if (object.type === "way") {
-        drawGeometry(object.geometry, {latMin, longMin, latMax, longMax});
+        drawGeometry(object.geometry, {latMin, longMin, latMax, longMax}, color);
       } else if (object.type === "relation") {
         object.members.forEach((member) => {
           if (member.type === "way") {
-            drawGeometry(member.geometry, {latMin, longMin, latMax, longMax});
+            drawGeometry(member.geometry, {latMin, longMin, latMax, longMax}, color);
           }
         })
       }
@@ -194,12 +114,18 @@ function drawGeometry(geoArray, {latMin, longMin, latMax, longMax}, fillColor=25
   endShape();
 }
 
+class TestCoordinates {
+  static coords = {
+    Taipei: [25.029928, 121.470337, 25.054501, 121.499004],
+    Durham: [35.985577, -78.913336, 36.004673, -78.888788],
+  }
+}
 
 ///////////////////////////////////////////////////////////
 // GLOBALS AND CONFIG
-
+const OFFLINE = true;
 const TIMEOUT = 6;  // unit = seconds
-const osmFetcher = new SlowFetcher(500);
+const REQUEST_DELAY = 3000;  // delay to play nice with OSM servers
 
 let done = false;
 
@@ -209,21 +135,22 @@ const myQueries = {
   green_space: `
     wr["leisure"~"park|garden"];
     wr["landuse"~"grass|forest|meadow|orchard"];`,
-  farm: `wr["landuse"~"farmyard|vineyard"]`,
-  industrial: `wr["landuse"~"industrial|quarry|brownfield|military|logging|landfill"]`
+  farm: `wr["landuse"~"farmyard|vineyard"];`,
+  industrial: `wr["landuse"~"industrial|quarry|brownfield|military|logging|landfill"];`
 }
 
 const coords = TestCoordinates.coords.Durham;
 
 ///////////////////////////////////////////////////////////
-// MAIN LOOP
+// APP LOGIC
 
+const osmFetcher = new SlowFetcher(REQUEST_DELAY);
 const [latMin, longMin, latMax, longMax] = coords;
 
 async function setup() {
   createCanvas(800, 800);
   background(240);
-  // noFill();
+  noFill();
   strokeWeight(0.5);
   console.log("loading...")
   // renderTile(coords, await fetchLayer(coords, [myQueries.building]));
@@ -235,4 +162,38 @@ async function setup() {
 function draw() {
   // Intentionally left blank
   // No `draw` loop is needed
+}
+
+
+async function fetchLayer(coords, queries) {
+  /*
+  coords: OSM-formatted array of coords, eg [25.029928, 121.470337, 25.054501, 121.499004]
+  queries: array of one or more Overpass API queries eg [`wr["building"];`, `wr["landuse"];`]
+  Return: json API response
+  */
+  if (OFFLINE) {
+    const response = await fetch("./data_durham.json");
+    return await response.json();
+  }
+  try {
+    const coordString = coords.join(",")
+    const osmQuery = "data=" + encodeURIComponent(`
+        [bbox:${coordString}][out:json][timeout:${TIMEOUT}];
+        (
+          ${queries.join('')}
+        );
+        out geom;`);
+
+    const response = await osmFetcher.fetch("https://overpass-api.de/api/interpreter", {
+      method: "POST",
+      body: osmQuery,
+    });
+
+    const json = await response.json();
+    return json;
+
+  } catch (error) {
+    console.log("Fetch from OSM failed");
+    throw error;
+  }
 }
